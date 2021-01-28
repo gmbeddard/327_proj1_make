00a991cef6093d48b3ed34d28985a3a231199bf5
gmbeddard <bree.beddard@me.com>
2021-01-28 16:36:11 -0500

    Pushing Project 1 to my git repo

--- main.cpp
+++ main.cpp
@@ -1,14 +1,9 @@
 //#includes are ALWAYS at top of file
+#include "myfunc.h"	// references myfunc.h so that the program knows how to find it
 #include <iostream>	//iostream is where std::cout and std::endl are defined
-
-std::string func()
-{
-	//say hello
-	return "hello";
-}
 
 int main()
 {
-	std::cout<<func()<<std::endl;
+	std::cout<<func()<<std::endl;		// include myfunc.h so knows what to do with this 
 	return 0;
 }
